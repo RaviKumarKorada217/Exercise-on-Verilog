f1869480c63b94d3b20089cab0188ba47d3a1c43
Ravi Kumar <124238054+RaviKumarKorada217@users.noreply.github.com>
2023-03-25 16:24:43 +0530

    Create 1. always (combinational).v

---
+++ 2. Verilog Language/4. Procedures/1. always (combinational).v
@@ -0,0 +1,14 @@
+// AND gate using assign and always
+
+module top_module(
+    input a, 
+    input b,
+    output wire out_assign,
+    output reg out_alwaysblock
+);
+    assign out_assign = a&b;
+    
+    always@(*)
+        out_alwaysblock = a&b;
+
+endmodule
